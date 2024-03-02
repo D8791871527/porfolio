a946fefae816394a79831c0ad3a45b7e99578d53
student Deepak <Dkumar20208958805345@gmail.com>
2024-03-02 13:59:01 -0800

    This is my portfolio

---
+++ main.js
@@ -0,0 +1,22 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer" ,""],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+var f=document.querySelector("h3");
+
+f.innerHTML="Thi is college student";
+f.style.color="red"
+f.style.backgroundColor="yellow"
+
+var r=document.getElementById("rn")
+r.style.height="40%"
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/D8791871527/porfolio.git
+// git push -u origin main
